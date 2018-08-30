5efaab22f1c5c554f908dfc380a46ccd63d47b38
shenjianlin <jianlin.shen@56qq.com>
2018-08-30 17:45:04 +0800

    初始化工程

---
+++ src/com/manzo/plugin/actions/SimpleGenerateCodeAction.java
@@ -0,0 +1,15 @@
+package com.manzo.plugin.actions;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+/**
+ * Created by shenjianlin on 2018/8/30.
+ */
+public class SimpleGenerateCodeAction extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        // TODO: insert action logic here
+    }
+}
